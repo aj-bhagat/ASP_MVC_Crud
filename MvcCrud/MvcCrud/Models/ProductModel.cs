5cfe5493d524460dfb5cbd475c6255ca3f3f1864
aj-bhagat <ajaybhagatm620@gmail.com>
2018-12-26 00:51:28 +0530

    ASP MVC CRUD

--- MvcCrud/MvcCrud/Models/ProductModel.cs
+++ MvcCrud/MvcCrud/Models/ProductModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 
@@ -8,6 +9,7 @@ namespace MvcCrud.Models
     public class ProductModel
     {
         public int ProductID { get; set; }
+        [DisplayName("Product Name")]
         public string ProductName { get; set; }
         public decimal Price { get; set; }
         public int Count { get; set; } 
